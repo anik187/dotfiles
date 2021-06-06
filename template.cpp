c219268b71dc544f16426627ec491a38174a2cc9
anik <anik@pc>
2021-06-07 00:07:54 +0600

    edited template file for cp

--- template.cpp
+++ template.cpp
@@ -1,38 +1,76 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
-typedef long long ll;
+typedef long int l32;
+typedef unsigned long int ul32;
+typedef long long int ll;
+typedef unsigned long long int  ull64;
+typedef pair<ll, ll> pll;
 typedef vector<ll> vll;
-typedef set<ll> sll;
-typedef pair<ll,ll> pll;
+typedef vector<string> vs;
+typedef vector<pll> vpll;
+typedef vector<vpll> VVI;
+typedef map<ll,ll> MPII;
+typedef set<ll> SETI;
+typedef multiset<ll> MSETI;
 
-
-#define mp make_pair
-#define pb push_back
 #define ff first
 #define ss second
+#define mp make_pair
+#define pb push_back
+#define MOD 1000000007
+#define FOR(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (ll i=j ; i>=k ; i-=in)
+#define fo(i, j) FOR(i, 0, j, 1)
+#define rfo(i, j) RFOR(i, j, 0, 1)
 #define all(cont) cont.begin(), cont.end()
 #define rall(cont) cont.rbegin(), cont.rend()
-#define fo(i,n) for (ll i = 0; i < n; i++)
-#define rep(i,a,b) for(ll i = a ; i<b ; i++)
-#define repr(i,a,b) for(ll i = a-1 ; i>=b ; i--)
-#define rfo(i,n) for (ll i = n - 1; i >= 0; i--)
-#define SORT(a) sort(all(a))
-#define REVERSE(a) sort(rall(a))
-ll gcd (ll a, ll b) { return b ? gcd (b, a % b) : a; }
-ll lcm (ll a, ll b) { return a / gcd(a, b) * b; }
+#define endl "\n"
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((ll)(x).size())
 
-void solve() {
-  
+#ifdef Anik
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull64 t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve(){
+	ll n,ans=0;
 }
 
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  ll t;
-  cin>>t;
-  while(t--)  
-  solve();
-  return 0;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifdef Anik
+		freopen("err.txt", "w", stderr);
+	#endif
+
+	ll t;
+	cin>>t;
+	fo(i,t)
+	solve();
+	return 0;
 }
